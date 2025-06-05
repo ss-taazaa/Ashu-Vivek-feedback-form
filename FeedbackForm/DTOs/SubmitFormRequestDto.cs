0f40abd45cddd1d39880ba4dfb30f2bf687316d8
ashu-kumar1 <ashutosh.kumar1@taazaa.com>
2025-06-05 11:54:15 +0530

    updated program.cs

---
+++ FeedbackForm/DTOs/SubmitFormRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace FeedbackForm.DTOs
+{
+    public class SubmitFormRequestDto
+    {
+        public string ShareableLink { get; set; }
+        public string RespondentId { get; set; }
+        public string RespondentName { get; set; }
+        public string RespondentEmail { get; set; }
+        public List<AnswerDto> Answers { get; set; }
+    }
+}
--- FeedbackForm/Program.cs
+++ FeedbackForm/Program.cs
@@ -1,68 +1,52 @@
-
 using FeedbackForm.Data;
 using FeedbackForm.Repositories.Implementations;
 using FeedbackForm.Repositories.Interfaces;
 using FeedbackForm.Services.Implementations;
 using FeedbackForm.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
-namespace FeedbackForm
-{
-    public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
     {
-        public static void Main(string[] args)
-        {
-            
-            
-            
-           var builder = WebApplication.CreateBuilder(args);
-
-          
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-            builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-            builder.Services.AddScoped<IFormService, FormService>();
-            builder.Services.AddScoped<IUserService, UserService>();
-
-            builder.Services.AddControllers()
-    .AddJsonOptions(x =>
-    {
-        x.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
-        x.JsonSerializerOptions.WriteIndented = true;
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        options.JsonSerializerOptions.WriteIndented = true;
     });
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-            builder.Services.AddCors(options =>
-            {
-                options.AddPolicy("AllowAll", policy =>
-                {
-                    policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
-                });
-            });
-            var app = builder.Build();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<IFormService, FormService>();
+builder.Services.AddScoped<IUserService, UserService>();
 
-            app.UseHttpsRedirection();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
 
-            app.UseAuthorization();
+var app = builder.Build();
 
-            app.UseCors("AllowAll");
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.UseCors("AllowAll");
+
+app.MapControllers();
+
+app.Run();
