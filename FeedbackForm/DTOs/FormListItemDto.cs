077bdb409e7cadf04b97bd180345ce3fd03abc6d
Vivek Kumar Verma <vivek.verma1@taazaa.com>
2025-06-05 11:50:57 +0530

    before response

---
+++ FeedbackForm/DTOs/FormListItemDto.cs
@@ -0,0 +1,16 @@
+﻿namespace FeedbackForm.DTOs
+{
+    public class FormListItemDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public string? ShareableLink { get; set; }
+        //public int SubmissionCount { get; set; }
+        public DateTime? PublishedOn { get; set; }
+        public DateTime? ClosedOn { get; set; }
+        public bool CanEdit { get; set; }
+        public bool CanClose { get; set; }
+    }
+}
